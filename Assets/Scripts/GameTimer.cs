caece16288bbc01d8bb35e63638336376960d0b7
GameWorldStudio <enzomazzarella1@gmail.com>
2021-06-20 22:01:42 +0200

    Ajout du projet

---
+++ Assets/Scripts/GameTimer.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameTimer : MonoBehaviour
+{
+
+    [SerializeField] private float totalTime;
+
+    private IEnumerator Timer()
+    {
+        while (!GameManager.gameFinish)
+        {
+            if (!WaveManager.waweTransition)
+            {
+                yield return new WaitForSeconds(1);
+                totalTime += 1;
+            }
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(Timer());
+    }
+}
